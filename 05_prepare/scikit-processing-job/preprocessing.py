12e339de3df11ac14e19b0ca4a08c77722391f70
Chris Fregly <chris@fregly.com>
2020-03-02 05:45:13 +0000

    updated adhoc processing with scikit

--- 05_prepare/scikit-processing-job/preprocessing.py
+++ 05_prepare/scikit-processing-job/preprocessing.py
@@ -1,4 +1,3 @@
-
 import argparse
 import os
 import warnings
@@ -6,13 +5,9 @@ import warnings
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelBinarizer, KBinsDiscretizer
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.compose import make_column_transformer
-
 from sklearn.exceptions import DataConversionWarning
-warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
 columns = ['age', 'education', 'major industry code', 'class of worker', 'num persons worked for employer',
            'capital gains', 'capital losses', 'dividends from stocks', 'income']
@@ -24,7 +19,7 @@ def print_shape(df):
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train-test-split-ratio', type=float, default=0.3)
+    parser.add_argument('--train-test-split-ratio', type=float, default=0.2)
     args, _ = parser.parse_known_args()
     
     print('Received arguments {}'.format(args))
@@ -32,6 +27,7 @@ if __name__=='__main__':
     input_data_path = os.path.join('/opt/ml/processing/input', 'census-income.csv')
     
     print('Reading input data from {}'.format(input_data_path))
+    
     df = pd.read_csv(input_data_path)
     df = pd.DataFrame(data=df, columns=columns)
     df.dropna(inplace=True)
@@ -44,7 +40,8 @@ if __name__=='__main__':
     split_ratio = args.train_test_split_ratio
     print('Splitting data into train and test sets with ratio {}'.format(split_ratio))
     X_train, X_test, y_train, y_test = train_test_split(df.drop('income', axis=1), df['income'], test_size=split_ratio, random_state=0)
-
+    # TODO:  Split again for validation
+    
     preprocess = make_column_transformer(
         (['age', 'num persons worked for employer'], KBinsDiscretizer(encode='onehot-dense', n_bins=10)),
         (['capital gains', 'capital losses', 'dividends from stocks'], StandardScaler()),
@@ -52,25 +49,39 @@ if __name__=='__main__':
     )
     print('Running preprocessing and feature engineering transformations')
     train_features = preprocess.fit_transform(X_train)
+#    validation_features = preprocess.fit_transform(X_validation)
     test_features = preprocess.transform(X_test)
     
     print('Train data shape after preprocessing: {}'.format(train_features.shape))
+#    print('Validation data shape after preprocessing: {}'.format(validation_features.shape))
     print('Test data shape after preprocessing: {}'.format(test_features.shape))
     
     train_features_output_path = os.path.join('/opt/ml/processing/train', 'train_features.csv')
     train_labels_output_path = os.path.join('/opt/ml/processing/train', 'train_labels.csv')
-    
+
+#    validation_features_output_path = os.path.join('/opt/ml/processing/validation', 'validation_features.csv')
+#    validation_labels_output_path = os.path.join('/opt/ml/processing/validation', 'validation_labels.csv')
+
     test_features_output_path = os.path.join('/opt/ml/processing/test', 'test_features.csv')
     test_labels_output_path = os.path.join('/opt/ml/processing/test', 'test_labels.csv')
     
     print('Saving training features to {}'.format(train_features_output_path))
     pd.DataFrame(train_features).to_csv(train_features_output_path, header=False, index=False)
-    
+
+#    print('Saving validation features to {}'.format(validation_features_output_path))
+#    pd.DataFrame(validation_features).to_csv(validation_features_output_path, header=False, index=False)
+
     print('Saving test features to {}'.format(test_features_output_path))
     pd.DataFrame(test_features).to_csv(test_features_output_path, header=False, index=False)
     
     print('Saving training labels to {}'.format(train_labels_output_path))
     y_train.to_csv(train_labels_output_path, header=False, index=False)
-    
+
+#    print('Saving validation labels to {}'.format(validation_labels_output_path))
+#    y_validation.to_csv(validation_labels_output_path, header=False, index=False)
+
     print('Saving test labels to {}'.format(test_labels_output_path))
     y_test.to_csv(test_labels_output_path, header=False, index=False)
+    
+    print('Complete')
+    
