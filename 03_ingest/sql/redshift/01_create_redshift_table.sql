c2ab8d74117af56eb492bf48590d824cf29845fa
Chris Fregly <chris@fregly.com>
2020-03-02 07:08:27 +0000

    updated length of redshift columns

--- 03_ingest/sql/redshift/01_create_redshift_table.sql
+++ 03_ingest/sql/redshift/01_create_redshift_table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE amazon_reviews(
-         marketplace varchar(1024),
-         customer_id varchar(1024),
-         review_id varchar(1024),
-         product_id varchar(1024),
-         product_parent varchar(1024),
+         marketplace varchar(2),
+         customer_id varchar(8),
+         review_id varchar(14),
+         product_id varchar(10),
+         product_parent varchar(9),
          product_title varchar(400),
-         product_category varchar(1024),
+         product_category varchar(24),
          star_rating int,
          helpful_votes int,
          total_votes int,
-         vine varchar(1024),
-         verified_purchase varchar(1024),
+         vine varchar(1),
+         verified_purchase varchar(1),
          review_headline varchar(128),
          review_body varchar(65536),
-         review_date varchar(1024)
+         review_date varchar(10)
 )
