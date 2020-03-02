fa642390f8e88127197d338c682acb4916bea43b
Antje Barth <23338582+antje@users.noreply.github.com>
2020-03-02 05:38:04 +0000

    updates

--- 03_ingest/sql/athena/04_analyze_items_with_short_reviews.sql
+++ 03_ingest/sql/athena/04_analyze_items_with_short_reviews.sql
@@ -1,4 +1,3 @@
 SELECT *
 FROM amazon_reviews_parquet
-WHERE product_category = 'Digital_Video_Download'
-        AND LENGTH(review_body) <= 20 LIMIT 500;
+WHERE product_category = 'Digital_Video_Download' LIMIT 500;
