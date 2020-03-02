7ed58e7b1ceade4a245a7d02868d148aac190405
Chris Fregly <chris@fregly.com>
2020-03-02 06:59:05 +0000

    updated column sizes based on length(), cleaned up, added comments

--- 03_ingest/sql/redshift/01_create_redshift_table.sql
+++ 03_ingest/sql/redshift/01_create_redshift_table.sql
@@ -4,14 +4,14 @@ CREATE TABLE amazon_reviews(
          review_id varchar(1024),
          product_id varchar(1024),
          product_parent varchar(1024),
-         product_title varchar(1024),
+         product_title varchar(400),
          product_category varchar(1024),
          star_rating int,
          helpful_votes int,
          total_votes int,
          vine varchar(1024),
          verified_purchase varchar(1024),
-         review_headline varchar(1024),
-         review_body varchar(1024),
+         review_headline varchar(128),
+         review_body varchar(65536),
          review_date varchar(1024)
 )
