676b27acdba1bfc0ee21d75990620c4bd00c68a6
Chris Fregly <chris@fregly.com>
2020-03-21 04:01:30 +0000

    pip install inside of the pytorch script

--- 06_train/src_bert/bert_reviews.py
+++ 06_train/src_bert/bert_reviews.py
@@ -7,11 +7,15 @@ from sklearn.metrics import accuracy_score, precision_score, classification_repo
 import sklearn
 from sklearn import metrics
 from sklearn.base import BaseEstimator, TransformerMixin
-import nltk
 import re
-import xgboost as xgb
-from xgboost import XGBClassifier
 import glob
+
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'simpletransformers'])
+
+
+import simpletransformers
 from simpletransformers.classification import ClassificationModel
 
 # Note:  header=None
@@ -35,6 +39,7 @@ if __name__ == '__main__':
     parser.add_argument('--model-type', type=str, default='bert')
     parser.add_argument('--model-name', type=str, default='bert-base-cased')
     parser.add_argument('--use-cuda', type=bool, default=False)
+    parser.add_argument('--backend', type=str, default='gloo')
     parser.add_argument('--train-data', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     parser.add_argument('--validation-data', type=str, default=os.environ['SM_CHANNEL_VALIDATION'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
@@ -42,13 +47,44 @@ if __name__ == '__main__':
     args, _ = parser.parse_known_args()   
     model_type = args.model_type
     model_name = args.model_name
-    usa_cuda = args.use_cuda
+    backend = args.backend
     train_data = args.train_data
     validation_data = args.validation_data    
     model_dir = args.model_dir
+
+################
+# From https://github.com/aws/sagemaker-python-sdk/issues/1110
+#    is_distributed = len(args.hosts) > 1 and args.backend is not None
+#    logger.debug("Distributed training - {}".format(is_distributed))
+#    use_cuda = args.num_gpus > 0
+#    logger.debug("Number of gpus available - {}".format(args.num_gpus))
+#    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+#    device = torch.device("cuda" if use_cuda else "cpu")
+#
+#    if is_distributed:
+#        # Initialize the distributed environment.
+#        world_size = len(args.hosts)
+#        os.environ['WORLD_SIZE'] = str(world_size)
+#        host_rank = args.hosts.index(args.current_host)
+#        os.environ['RANK'] = str(host_rank)
+#        dist.init_process_group(backend=args.backend, rank=host_rank, world_size=world_size)
+#        logger.info('Initialized the distributed environment: \'{}\' backend on {} nodes. '.format(
+#            args.backend, dist.get_world_size()) + 'Current host rank is {}. Number of gpus: {}'.format(
+#            dist.get_rank(), args.num_gpus))
+###############
+
+#    if args.distributed:
+#        # Initialize the distributed environment.
+#        world_size = len(args.hosts)
+#        os.environ['WORLD_SIZE'] = str(world_size)
+#        host_rank = args.hosts.index(args.current_host)
+#        dist.init_process_group(backend=backend, rank=host_rank)
     
 #    X_train, y_train = load_dataset(train_data, ',', header=None)
 #    X_validation, y_validation = load_dataset(validation_data, ',', header=None)
+
+
+    # TODO:  Change this to use SM_CHANNEL_TRAIN, etc
 
     df = pd.read_csv('./data/amazon_reviews_us_Digital_Software_v1_00.tsv.gz', 
                  delimiter='\t', 
--- 06_train/src_bert/test-local.sh
+++ 06_train/src_bert/test-local.sh
@@ -1,3 +1,3 @@
-python copy_data_locally.py
+#python copy_data_locally.py
 
-SM_CHANNEL_TRAIN=data/train SM_CHANNEL_VALIDATION=data/validation SM_MODEL_DIR=. python xgboost_reviews_bert.py --model-type=distilbert --model-name=distilbert-base-cased
+SM_CHANNEL_TRAIN=data/train SM_CHANNEL_VALIDATION=data/validation SM_MODEL_DIR=. python bert_reviews.py --model-type=distilbert --model-name=distilbert-base-cased --distributed=False
