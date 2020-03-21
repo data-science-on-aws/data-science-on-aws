74be3fa345a3d760d540e4853ae383097d11038f
Chris Fregly <chris@fregly.com>
2020-03-21 05:08:58 +0000

    pytorch dist

--- 06_train/src_bert/bert_reviews.py
+++ 06_train/src_bert/bert_reviews.py
@@ -14,6 +14,7 @@ import subprocess
 import sys
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'simpletransformers'])
 import torch
+import torch.distributed as dist
 import torch.utils.data
 import torch.utils.data.distributed
 
