dee177fea6e120aeb842e55a0a21fc765aa34019
Chris Fregly <chris@fregly.com>
2020-04-06 01:34:28 +0000

    added debugging and experiments

--- 06_train/src/test-local.sh
+++ 06_train/src/test-local.sh
@@ -1,4 +1,4 @@
-python copy_data_locally.py
-
-SM_CHANNEL_TRAIN=feature-store/amazon-reviews/csv/balanced-tfidf-without-header/train SM_CHANNEL_VALIDATION=feature-store/amazon-reviews/csv/balanced-tfidf-without-header/validation SM_MODEL_DIR=. python xgboost_reviews.py 
-# --num-rounds=10
+rm -rf model/
+rm -rf output/
+#SM_INPUT_DATA_CONFIG={\"train\":{\"TrainingInputMode\":\"Pipe\"}} 
+SM_CURRENT_HOST=blah SM_NUM_GPUS=0 SM_HOSTS={\"hosts\":\"blah\"} SM_MODEL_DIR=model/ SM_OUTPUT_DATA_DIR=output/ SM_CHANNEL_TRAIN=data/train SM_CHANNEL_VALIDATION=data/validation SM_CHANNEL_TEST=data/test python tf_bert_reviews.py --use-xla=False --use-amp=False --train-batch-size=8 --validation-batch-size=8 --test-batch-size=8 --epochs=2 --train-steps-per-epoch=10 --validation-steps=10 --test-steps=10 --max-seq-length=128 --freeze-bert-layer=False --enable-sagemaker-debugger=True
