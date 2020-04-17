38a4f65dd2d122c7bb00b78f90796b6b27b046cc
Chris Fregly <chris@fregly.com>
2020-04-17 00:57:46 +0000

    using latest tf bert

--- 06_train/src/tf_bert_reviews.py
+++ 06_train/src/tf_bert_reviews.py
@@ -9,7 +9,7 @@ import subprocess
 import sys
 import os
 import tensorflow as tf
-#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tensorflow==2.0.0'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tensorflow==2.1.0'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'transformers==2.8.0'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sagemaker-tensorflow==2.1.0.1.0.0'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'smdebug==0.7.2'])
@@ -62,12 +62,16 @@ def file_based_input_dataset_builder(channel,
       "input_mask": tf.io.FixedLenFeature([max_seq_length], tf.int64),
       "segment_ids": tf.io.FixedLenFeature([max_seq_length], tf.int64),
       "label_ids": tf.io.FixedLenFeature([], tf.int64),
-#      "is_real_example": tf.io.FixedLenFeature([], tf.int64),
     }
 
     def _decode_record(record, name_to_features):
         """Decodes a record to a TensorFlow example."""
-        return tf.io.parse_single_example(record, name_to_features)
+        record = tf.io.parse_single_example(record, name_to_features)
+        # TODO:  wip/bert/bert_attention_head_view/train.py
+        # Convert input_ids into input_tokens with DistilBert vocabulary 
+        #  if hook.get_collections()['all'].save_config.should_save_step(modes.EVAL, hook.mode_steps[modes.EVAL]):
+#              hook._write_raw_tensor_simple("input_tokens", input_tokens)
+        return record
     
     dataset = dataset.apply(
         tf.data.experimental.map_and_batch(
@@ -89,78 +93,87 @@ def file_based_input_dataset_builder(channel,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--train-data', 
+    parser.add_argument('--train_data', 
                         type=str, 
                         default=os.environ['SM_CHANNEL_TRAIN'])
-    parser.add_argument('--validation-data', 
+    parser.add_argument('--validation_data', 
                         type=str, 
                         default=os.environ['SM_CHANNEL_VALIDATION'])
-    parser.add_argument('--test-data',
+    parser.add_argument('--test_data',
                         type=str,
                         default=os.environ['SM_CHANNEL_TEST'])
-    parser.add_argument('--model-dir', 
-                        type=str, 
-                        default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--output-data-dir',
+    # This points to the S3 location - this should not be used by our code
+    # We should use /opt/ml/model/ instead
+#     parser.add_argument('--model_dir', 
+#                         type=str, 
+#                         default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default=os.environ['SM_OUTPUT_DIR'])
+    # This is unused
+    parser.add_argument('--output_data_dir',
                         type=str,
                         default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--hosts', 
                         type=list, 
                         default=json.loads(os.environ['SM_HOSTS']))
-    parser.add_argument('--current-host', 
+    parser.add_argument('--current_host', 
                         type=str, 
                         default=os.environ['SM_CURRENT_HOST'])    
-    parser.add_argument('--num-gpus', 
+    parser.add_argument('--num_gpus', 
                         type=int, 
                         default=os.environ['SM_NUM_GPUS'])
-    parser.add_argument('--use-xla',
-                        type=bool,
+    parser.add_argument('--use_xla',
+                        type=eval,
                         default=False)
-    parser.add_argument('--use-amp',
-                        type=bool,
+    parser.add_argument('--use_amp',
+                        type=eval,
                         default=False)
-    parser.add_argument('--max-seq-length',
+    parser.add_argument('--max_seq_length',
                         type=int,
                         default=128)
-    parser.add_argument('--train-batch-size',
+    parser.add_argument('--train_batch_size',
                         type=int,
                         default=128)
-    parser.add_argument('--validation-batch-size',
+    parser.add_argument('--validation_batch_size',
                         type=int,
                         default=256)
-    parser.add_argument('--test-batch-size',
+    parser.add_argument('--test_batch_size',
                         type=int,
                         default=256)
     parser.add_argument('--epochs',
                         type=int,
                         default=2)
-    parser.add_argument('--train-steps-per-epoch',
+    parser.add_argument('--learning_rate',
+                        type=float,
+                        default=0.00003)
+    parser.add_argument('--epsilon',
+                        type=float,
+                        default=0.00000001)
+    parser.add_argument('--train_steps_per_epoch',
                         type=int,
-                        default=100)
-    parser.add_argument('--validation-steps',
+                        default=1000)
+    parser.add_argument('--validation_steps',
                         type=int,
-                        default=100)
-    parser.add_argument('--test-steps',
+                        default=1000)
+    parser.add_argument('--test_steps',
                         type=int,
-                        default=100)
-    parser.add_argument('--freeze-bert-layer',
-                        type=bool,
+                        default=1000)
+    parser.add_argument('--freeze_bert_layer',
+                        type=eval,
                         default=False)
-    parser.add_argument('--enable-sagemaker-debugger',
-                        type=bool,
+    parser.add_argument('--enable_sagemaker_debugger',
+                        type=eval,
                         default=False)
-    parser.add_argument('--run-validation',
-                        type=bool,
+    parser.add_argument('--run_validation',
+                        type=eval,
                         default=False)    
-    parser.add_argument('--run-test',
-                        type=bool,
+    parser.add_argument('--run_test',
+                        type=eval,
                         default=False)    
-    parser.add_argument('--run-sample-predictions',
-                        type=bool,
-                        default=False)    
-#     parser.add_argument('--disable-eager-execution',
-#                         type=bool,
-#                         default=False) 
+    parser.add_argument('--run_sample_predictions',
+                        type=eval,
+                        default=False)
     
     args, _ = parser.parse_known_args()
     print("Args:") 
@@ -171,29 +184,62 @@ if __name__ == '__main__':
     pprint.pprint(dict(env_var), width = 1) 
 
     train_data = args.train_data
+    print('train_data {}'.format(train_data))
     validation_data = args.validation_data
+    print('validation_data {}'.format(validation_data))
     test_data = args.test_data
-    model_dir = args.model_dir
-    output_data_dir = args.output_data_dir
+    print('test_data {}'.format(test_data))    
+
+#    model_dir = args.model_dir
+#    print('model_dir {}'.format(model_dir))    
+    local_model_dir = os.environ['SM_MODEL_DIR']
+
+    output_dir = args.output_dir
+    print('output_dir {}'.format(output_dir))    
+
+    # This is unused
+#    output_data_dir = args.output_data_dir
+#    print('output_data_dir {}'.format(output_data_dir))    
     hosts = args.hosts
+    print('hosts {}'.format(hosts))    
     current_host = args.current_host
+    print('current_host {}'.format(current_host))    
     num_gpus = args.num_gpus
+    print('num_gpus {}'.format(num_gpus))
     use_xla = args.use_xla
+    print('use_xla {}'.format(use_xla))    
     use_amp = args.use_amp
+    print('use_amp {}'.format(use_amp))    
     max_seq_length = args.max_seq_length
+    print('max_seq_length {}'.format(max_seq_length))    
     train_batch_size = args.train_batch_size
+    print('train_batch_size {}'.format(train_batch_size))    
     validation_batch_size = args.validation_batch_size
+    print('validation_batch_size {}'.format(validation_batch_size))    
     test_batch_size = args.test_batch_size
+    print('test_batch_size {}'.format(test_batch_size))    
     epochs = args.epochs
+    print('epochs {}'.format(epochs))    
+    learning_rate = args.learning_rate
+    print('learning_rate {}'.format(learning_rate))    
+    epsilon = args.epsilon
+    print('epsilon {}'.format(epsilon))    
     train_steps_per_epoch = args.train_steps_per_epoch
+    print('train_steps_per_epoch {}'.format(train_steps_per_epoch))    
     validation_steps = args.validation_steps
+    print('validation_steps {}'.format(validation_steps))    
     test_steps = args.test_steps
+    print('test_steps {}'.format(test_steps))    
     freeze_bert_layer = args.freeze_bert_layer
+    print('freeze_bert_layer {}'.format(freeze_bert_layer))    
     enable_sagemaker_debugger = args.enable_sagemaker_debugger
+    print('enable_sagemaker_debugger {}'.format(enable_sagemaker_debugger))    
     run_validation = args.run_validation
+    print('run_validation {}'.format(run_validation))    
     run_test = args.run_test
+    print('run_test {}'.format(run_test))    
     run_sample_predictions = args.run_sample_predictions
-#    disable_eager_execution = args.disable_eager_execution    
+    print('run_sample_predictions {}'.format(run_sample_predictions))    
 
     # Determine if PipeMode is enabled 
     pipe_mode_str = os.environ.get('SM_INPUT_DATA_CONFIG', '')
@@ -201,23 +247,20 @@ if __name__ == '__main__':
     print('Using pipe_mode: {}'.format(pipe_mode))
  
     # Model Output 
-    transformer_pretrained_model_path = os.path.join(model_dir, 'transformer/pretrained')
-    os.makedirs(transformer_pretrained_model_path, exist_ok=True)
+    transformer_fine_tuned_model_path = os.path.join(local_model_dir, 'transformers/fine-tuned/')
+    os.makedirs(transformer_fine_tuned_model_path, exist_ok=True)
 
     # SavedModel Output
-    tensorflow_saved_model_path = os.path.join(model_dir, 'saved_model/0')
+    tensorflow_saved_model_path = os.path.join(local_model_dir, 'tensorflow/saved_model/0')
     os.makedirs(tensorflow_saved_model_path, exist_ok=True)
 
     # Tensorboard Logs 
-    tensorboard_logs_path = os.path.join(output_data_dir, 'tensorboard') 
+    tensorboard_logs_path = os.path.join(output_dir, 'tensorboard') 
     os.makedirs(tensorboard_logs_path, exist_ok=True)
 
-#     print('disable_eager_execution {}'.format(disable_eager_execution))
-#     if disable_eager_execution: 
-#         tf.compat.v1.disable_eager_execution()        
-        
-#    distributed_strategy = tf.distribute.MirroredStrategy()
-    distributed_strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+    distributed_strategy = tf.distribute.MirroredStrategy()
+    # smdebug currently (0.7.2) does not support MultiWorkerMirroredStrategy()
+    # distributed_strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
     with distributed_strategy.scope():
         tf.config.optimizer.set_jit(use_xla)
         tf.config.optimizer.set_experimental_options({"auto_mixed_precision": use_amp})
@@ -260,34 +303,35 @@ if __name__ == '__main__':
         if not tokenizer or not model or not config:
             print('Not properly initialized...')
 
-        optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, epsilon=epsilon)
+        print('** use_amp {}'.format(use_amp))        
         if use_amp:
             # loss scaling is currently required when using mixed precision
             optimizer = tf.keras.mixed_precision.experimental.LossScaleOptimizer(optimizer, 'dynamic')
 
         callbacks = []
+        print('enable_sagemaker_debugger {}'.format(enable_sagemaker_debugger))
         if enable_sagemaker_debugger:
+            print('*** DEBUGGING ***')
             import smdebug.tensorflow as smd
-            callback = smd.KerasHook(out_dir=output_data_dir,
-                                     export_tensorboard=True,        
-                                     tensorboard_dir=tensorboard_logs_path,
-                                     save_config=smd.SaveConfig(save_interval=100),
-#                                     save_all=True,
-                                     include_collections=['metrics', 
-                                                          'losses', 
-                                                          'sm_metrics'],
-                                     include_workers='all')
+            # This assumes that we specified debugger_hook_config
+            callback = smd.KerasHook.create_from_json_file()
+            print(callback)
+            print('*** CALLBACK {} ***'.format(callback))
             callbacks.append(callback)
             optimizer = callback.wrap_optimizer(optimizer)
-        else:
-            callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_logs_path)
-            callbacks.append(callback)
+
+        callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_logs_path)
+        callbacks.append(callback)
             
+        print('*** OPTIMIZER {} ***'.format(optimizer))
+        
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
 
         model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
         print('Trained model {}'.format(model))
+        print(model.summary())
 
         if run_validation:
             validation_data_filenames = glob(os.path.join(validation_data, '*.tfrecord'))
@@ -302,7 +346,10 @@ if __name__ == '__main__':
                 epochs=epochs,
                 steps_per_epoch=validation_steps,
                 max_seq_length=max_seq_length).map(select_data_and_label_from_record)
-
+            
+            # HACK:  trim the Validation dataset down to equal the number of validation steps to workaround PipeMode issue
+            validation_dataset = validation_dataset.take(validation_steps)
+            
             train_and_validation_history = model.fit(train_dataset,
                                                      shuffle=True,
                                                      epochs=epochs,
@@ -340,12 +387,16 @@ if __name__ == '__main__':
 
             
         # Save the fine-tuned Transformers Model
-        model.save_pretrained(transformer_pretrained_model_path)
+        print('transformer_fine_tuned_model_path {}'.format(transformer_fine_tuned_model_path))   
+
+        model.save_pretrained(transformer_fine_tuned_model_path)
+
         # Save the TensorFlow SavedModel
+        print('tensorflow_saved_model_path {}'.format(tensorflow_saved_model_path))   
         model.save(tensorflow_saved_model_path, save_format='tf')
 
     if run_sample_predictions:
-        loaded_model = TFDistilBertForSequenceClassification.from_pretrained(transformer_pretrained_model_path,
+        loaded_model = TFDistilBertForSequenceClassification.from_pretrained(transformer_fine_tuned_model_path,
                                                                        id2label={
                                                                         0: 1,
                                                                         1: 2,
