fb78c5720b9a655d5d3e635c088d458cc275be69
Chris Fregly <chris@fregly.com>
2020-04-22 02:56:11 +0000

    changed region

--- 10_kubeflow/src/mnist/src/tfjoblaunch_args_provider.py
+++ 10_kubeflow/src/mnist/src/tfjoblaunch_args_provider.py
@@ -23,7 +23,7 @@ def tfjoblauncher_args(step, s3bucketexportpath, args):
                         "env": [
                             {
                                 "name": "AWS_REGION",
-                                "value": "us-west-2"
+                                "value": "us-east-1"
                             },
                             {
                                 "name": "AWS_ACCESS_KEY_ID",
@@ -74,7 +74,7 @@ def tfjoblauncher_args(step, s3bucketexportpath, args):
                         "env": [
                             {
                                 "name": "AWS_REGION",
-                                "value": "us-west-2"
+                                "value": "us-east-1"
                             },
                             {
                                 "name": "AWS_ACCESS_KEY_ID",
