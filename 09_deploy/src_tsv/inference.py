725a3ffd36e305d5819e294c7d18c861904d8441
Chris Fregly <chris@fregly.com>
2020-06-09 01:12:41 +0000

    updated infernece code to use newer transformers api

--- 09_deploy/src_tsv/inference.py
+++ 09_deploy/src_tsv/inference.py
@@ -28,36 +28,20 @@ def input_handler(data, context):
         if (len(data_str_split) >= review_body_column_idx_tsv):
             print(data_str_split[review_body_column_idx_tsv])
 
-        tokens_a = tokenizer.tokenize(data_str_split[review_body_column_idx_tsv])
+        text_input = data_str_split[review_body_column_idx_tsv]
+        tokens = tokenizer.tokenize(text_input)
 
-        # Account for [CLS] and [SEP] with "- 2"
-        if len(tokens_a) > max_seq_length - 2:
-            tokens_a = tokens_a[0:(max_seq_length - 2)]
+        encode_plus_tokens = tokenizer.encode_plus(text_input,
+                                                   pad_to_max_length=True,
+                                                   max_length=max_seq_length)
 
-        tokens = []  
-        segment_ids = []
-        tokens.append("[CLS]")
-        segment_ids.append(0)
-        for token in tokens_a:
-            tokens.append(token)
-            segment_ids.append(0)  
-        tokens.append("[SEP]")
-        segment_ids.append(0)
-
-        input_ids = tokenizer.convert_tokens_to_ids(tokens)
-
-        input_mask = [1] * len(input_ids)
-
-        # Zero-pad up to the sequence length.
-        while len(input_ids) < max_seq_length:
-            input_ids.append(0)
-            input_mask.append(0)
-            segment_ids.append(0)
-
-        assert len(input_ids) == max_seq_length
-        assert len(input_mask) == max_seq_length
-        assert len(segment_ids) == max_seq_length
-
+        # Convert the text-based tokens to ids from the pre-trained BERT vocabulary
+        input_ids = encode_plus_tokens['input_ids']
+        # Specifies which tokens BERT should pay attention to (0 or 1)
+        input_mask = encode_plus_tokens['attention_mask']
+        # Segment Ids are always 0 for single-sequence tasks (or 1 if two-sequence tasks)
+        segment_ids = [0] * max_seq_length
+    
         transformed_instance = { 
                                  "input_ids": input_ids, 
                                  "input_mask": input_mask, 
