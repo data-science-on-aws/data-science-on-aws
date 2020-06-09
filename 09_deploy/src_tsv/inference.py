6377cabab124379eea0812b2a17fc2fbc46414f1
Chris Fregly <chris@fregly.com>
2020-06-09 03:34:47 +0000

    cleaned up logging

--- 09_deploy/src_tsv/inference.py
+++ 09_deploy/src_tsv/inference.py
@@ -12,19 +12,13 @@ tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
 def input_handler(data, context):
     transformed_instances = []
-    print(type(data))
-    print(data)
 
     for instance in data:
-        print(type(instance))
-        print(instance)
 
         data_str = instance.decode('utf-8')
-        print(type(data_str))
-        print(data_str)
 
         data_str_split = data_str.split('\t')
-        print(len(data_str_split))
+#        print(len(data_str_split))
         if (len(data_str_split) >= review_body_column_idx_tsv):
             print(data_str_split[review_body_column_idx_tsv])
 
@@ -51,19 +45,15 @@ def input_handler(data, context):
         transformed_instances.append(transformed_instance)
 
     transformed_data = {"instances": transformed_instances}
-    print(transformed_data)
 
     return json.dumps(transformed_data)
 
 
 def output_handler(response, context):
-    print(type(response))
-    print(response)
-
     response_json = response.json()
 
-    print(type(response_json))
-    print(response_json)
+#     print(type(response_json))
+#     print(response_json)
 
     log_probabilities = response_json["predictions"]
 
@@ -74,9 +64,9 @@ def output_handler(response, context):
         predicted_class_idx = tf.argmax(softmax, axis=-1, output_type=tf.int32)
         predicted_class = classes[predicted_class_idx]
         predicted_classes.append(predicted_class)
-
-    print(predicted_classes)
-    predicted_classes_json = json.dumps(predicted_classes)
+    
+    predicted_classes_json = json.dumps(predicted_classes)    
+    print(predicted_classes_json)
 
     response_content_type = context.accept_header
 
