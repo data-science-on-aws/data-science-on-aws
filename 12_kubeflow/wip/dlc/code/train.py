e0c3933f3cc2010b7af341f5e65bfd975e801878
Antje Barth <23338582+antje@users.noreply.github.com>
2020-10-28 21:15:22 +0000

    updated train

--- 12_kubeflow/wip/dlc/code/train.py
+++ 12_kubeflow/wip/dlc/code/train.py
@@ -539,7 +539,7 @@ if __name__ == '__main__':
 
         import csv
 
-        df_test_reviews = pd.read_csv('./test_data/amazon_reviews_us_Digital_Software_v1_00.tsv.gz', 
+        df_test_reviews = pd.read_csv('/opt/ml/input/amazon_reviews_us_Digital_Software_v1_00.tsv.gz', 
                                         delimiter='\t', 
                                         quoting=csv.QUOTE_NONE,
                                         compression='gzip')[['review_body', 'star_rating']]
