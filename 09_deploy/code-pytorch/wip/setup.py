26318c32adac0e5552a5329f745fff553d9b6e0a
Chris Fregly <chris@fregly.com>
2021-01-03 04:36:23 +0000

    cleaned up

--- 09_deploy/code-pytorch/wip/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='sagemaker-bert-example',
-      version='1.0',
-      description='SageMaker Example for Bert.',
-      author='sofian',
-      author_email='hamitis@amazon.com',
-      packages=find_packages(exclude=('tests', 'docs')))
