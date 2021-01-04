733479eef5a3ab0eb6261e90c05d1a7063849b69
Antje Barth <23338582+antje@users.noreply.github.com>
2021-01-04 06:04:23 +0000

    updated new project folder

--- 10_pipeline/copy-local-project-to-workshop.sh
+++ 10_pipeline/copy-local-project-to-workshop.sh
@@ -12,9 +12,9 @@ LOCAL_PROJECT_DEPLOY_PATH='/home/sagemaker-user/dsoaws-16093790406854542-p-zh1fc
 ##### WORKSHOP PATH TO PROJECT CODE
 ############################################################################################
 
-WORKSHOP_PROJECT_BUILD_PATH='/home/sagemaker-user/workshop/10_pipeline/project-dsoaws-p-ibxfjw9nuim7/sagemaker-project-dsoaws-p-ibxfjw9nuim7-modelbuild'
+WORKSHOP_PROJECT_BUILD_PATH='/home/sagemaker-user/workshop/10_pipeline/sagemaker/sagemaker-project-modelbuild'
 
-WORKSHOP_PROJECT_DEPLOY_PATH='/home/sagemaker-user/workshop/10_pipeline/project-dsoaws-p-ibxfjw9nuim7/sagemaker-project-dsoaws-p-ibxfjw9nuim7-modeldeploy'
+WORKSHOP_PROJECT_DEPLOY_PATH='/home/sagemaker-user/workshop/10_pipeline/sagemaker/sagemaker-project-modeldeploy'
 
 ############################################################################################
 ##### COPY LOCAL TO WORKSHOP
@@ -22,32 +22,9 @@ WORKSHOP_PROJECT_DEPLOY_PATH='/home/sagemaker-user/workshop/10_pipeline/project-
 
 echo "Copy'ing files from local projects folder to workshop repo..."
 
-cp -r $LOCAL_PROJECT_BUILD_PATH/. $WORKSHOP_PROJECT_BUILD_PATH/
+cp -R $LOCAL_PROJECT_BUILD_PATH/* $WORKSHOP_PROJECT_BUILD_PATH/
 
-#cp $LOCAL_PROJECT_BUILD_PATH/codebuid-buildspec.yml $WORKSHOP_PROJECT_BUILD_PATH/codebuild-buildspec.yml
-
-#cp $LOCAL_PROJECT_BUILD_PATH/setup.py $WORKSHOP_PROJECT_BUILD_PATH/setup.py
-
-#cp $LOCAL_PROJECT_BUILD_PATH/99_Create_Sagemaker_Pipeline_BERT_Reviews_MLOps.ipynb $WORKSHOP_PROJECT_BUILD_PATH/99_Create_Sagemaker_Pipeline_BERT_Reviews_MLOps.ipynb 
-
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/run_pipeline.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/run_pipeline.py 
-
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/evaluate_model_metrics.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/evaluate_model_metrics.py 
-
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/inference.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/inference.py 
-
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/pipeline.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/pipeline.py 
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/preprocess-scikit-text-to-bert-feature-store.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/preprocess-scikit-text-to-bert-feature-store.py 
-
-#cp $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/tf_bert_reviews.py $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/tf_bert_reviews.py
-
-###########################################################################################
-
-cp -r $LOCAL_PROJECT_DEPLOY_PATH/. $WORKSHOP_PROJECT_DEPLOY_PATH/
-
-#cp $LOCAL_PROJECT_DEPLOY_PATH/prod-config.json $WORKSHOP_PROJECT_DEPLOY_PATH/prod-config.json
-
-#cp $LOCAL_PROJECT_DEPLOY_PATH/staging-config.json $WORKSHOP_PROJECT_DEPLOY_PATH/staging-config.json
+cp -R $LOCAL_PROJECT_DEPLOY_PATH/* $WORKSHOP_PROJECT_DEPLOY_PATH/
 
 ############################################################################################
 echo "Done."
--- 10_pipeline/copy-workshop-project-to-local.sh
+++ 10_pipeline/copy-workshop-project-to-local.sh
@@ -12,41 +12,19 @@ LOCAL_PROJECT_DEPLOY_PATH='/home/sagemaker-user/dsoaws-16093790406854542-p-zh1fc
 ##### WORKSHOP PATH TO PROJECT CODE
 ############################################################################################
 
-WORKSHOP_PROJECT_BUILD_PATH='/home/sagemaker-user/workshop/10_pipeline/project-dsoaws-p-ibxfjw9nuim7/sagemaker-project-dsoaws-p-ibxfjw9nuim7-modelbuild'
+WORKSHOP_PROJECT_BUILD_PATH='/home/sagemaker-user/workshop/10_pipeline/sagemaker/sagemaker-project-modelbuild'
 
-WORKSHOP_PROJECT_DEPLOY_PATH='/home/sagemaker-user/workshop/10_pipeline/project-dsoaws-p-ibxfjw9nuim7/sagemaker-project-dsoaws-p-ibxfjw9nuim7-modeldeploy'
+WORKSHOP_PROJECT_DEPLOY_PATH='/home/sagemaker-user/workshop/10_pipeline/sagemaker/sagemaker-project-modeldeploy'
 
 ############################################################################################
 ##### COPY WORKSHOP TO LOCAL
 ############################################################################################
 
 echo "Copy'ing files from workshop repo to local projects folder..."
-cp -r $WORKSHOP_PROJECT_BUILD_PATH/. $LOCAL_PROJECT_BUILD_PATH/
 
-#cp $WORKSHOP_PROJECT_BUILD_PATH/codebuild-buildspec.yml $LOCAL_PROJECT_BUILD_PATH/codebuid-buildspec.yml
-#cp $WORKSHOP_PROJECT_BUILD_PATH/setup.py $LOCAL_PROJECT_BUILD_PATH/setup.py
+cp -R $WORKSHOP_PROJECT_BUILD_PATH/* $LOCAL_PROJECT_BUILD_PATH/
 
-#cp $WORKSHOP_PROJECT_BUILD_PATH/99_Create_Sagemaker_Pipeline_BERT_Reviews_MLOps.ipynb $LOCAL_PROJECT_BUILD_PATH/99_Create_Sagemaker_Pipeline_BERT_Reviews_MLOps.ipynb
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/run_pipeline.py $LOCAL_PROJECT_BUILD_PATH/pipelines/run_pipeline.py
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/evaluate_model_metrics.py $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/evaluate_model_metrics.py
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/inference.py $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/inference.py
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/pipeline.py $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/pipeline.py
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/preprocess-scikit-text-to-bert-feature-store.py $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/preprocess-scikit-text-to-bert-feature-store.py
-
-#cp $WORKSHOP_PROJECT_BUILD_PATH/pipelines/dsoaws/tf_bert_reviews.py $LOCAL_PROJECT_BUILD_PATH/pipelines/dsoaws/tf_bert_reviews.py
-
-###########################################################################################
-
-cp -r $WORKSHOP_PROJECT_DEPLOY_PATH/. $LOCAL_PROJECT_DEPLOY_PATH/
-
-#cp $WORKSHOP_PROJECT_DEPLOY_PATH/prod-config.json $LOCAL_PROJECT_DEPLOY_PATH/prod-config.json 
-
-#cp $WORKSHOP_PROJECT_DEPLOY_PATH/staging-config.json LOCAL_PROJECT_DEPLOY_PATH/staging-config.json
+cp -R $WORKSHOP_PROJECT_DEPLOY_PATH/* $LOCAL_PROJECT_DEPLOY_PATH/
 
 ############################################################################################
 
