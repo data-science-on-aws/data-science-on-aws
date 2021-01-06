89963316392a76266404f6e0d95d180e90f5f0be
Antje Barth <23338582+antje@users.noreply.github.com>
2021-01-06 08:53:46 +0000

    updated role

--- 10_pipeline/kubeflow/preprocess-scikit-text-to-bert-feature-store.py
+++ 10_pipeline/kubeflow/preprocess-scikit-text-to-bert-feature-store.py
@@ -54,11 +54,19 @@ print('caller_identity: {}'.format(caller_identity))
 assumed_role = caller_identity['Arn']
 print('(assumed_role) caller_identity_arn: {}'.format(assumed_role))
 
-# Derived and inspired by these since sagemaker.get_execution_role() doesn't work properly in our case:
-#     https://github.com/aws/sagemaker-python-sdk/blob/1fdefe06068e5eaf9f63287737d55db96ecc12cf/src/sagemaker/session.py#L3509
-#     https://github.com/aws/sagemaker-python-sdk/issues/300    
-role = re.sub(r"^(.+)sts::(\d+):assumed-role/(.+?)/.*$", r"\1iam::\2:role/service-role/\3", assumed_role)
-print('caller_identity (assume_role) with :role/service-role/ instead of :role/ {}'.format(role))
+if "AmazonSageMakerServiceCatalogProductsUseRole" in assumed_role:
+    role = re.sub(r"^(.+)sts::(\d+):assumed-role/(.+?)/.*$", r"\1iam::\2:role/service-role/\3", assumed_role)
+    print('caller_identity (assume_role) with :role/service-role/ instead of :role/ {}'.format(role))
+else:
+    role = re.sub(r"^(.+)sts::(\d+):assumed-role/(.+?)/.*$", r"\1iam::\2:role/\3", assumed_role)
+    print('caller_identity (assume_role) with :role/service-role/ instead of :role/ {}'.format(role))      
+    
+
+# # Derived and inspired by these since sagemaker.get_execution_role() doesn't work properly in our case:
+# #     https://github.com/aws/sagemaker-python-sdk/blob/1fdefe06068e5eaf9f63287737d55db96ecc12cf/src/sagemaker/session.py#L3509
+# #     https://github.com/aws/sagemaker-python-sdk/issues/300    
+# role = re.sub(r"^(.+)sts::(\d+):assumed-role/(.+?)/.*$", r"\1iam::\2:role/service-role/\3", assumed_role)
+# print('caller_identity (assume_role) with :role/service-role/ instead of :role/ {}'.format(role))
 
 # try:
 #     role = sagemaker.get_execution_role()
