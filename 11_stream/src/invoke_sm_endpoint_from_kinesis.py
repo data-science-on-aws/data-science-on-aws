631777452615d66111611f92adcec5b280c5406b
Chris Fregly <chris@fregly.com>
2021-11-30 22:16:15 +0000

    updated to Predictor

--- 11_stream/src/invoke_sm_endpoint_from_kinesis.py
+++ 11_stream/src/invoke_sm_endpoint_from_kinesis.py
@@ -48,23 +48,24 @@ def lambda_handler(event, context):
 
         inputs = [{"features": [review_body]}]
             
-        response = predictor.predict(inputs)
-        print("response: {}".format(response))
-
-        predicted_classes_str = response["Body"].read().decode()
-        predicted_classes_json = json.loads(predicted_classes_str)
-
-        predicted_classes = predicted_classes_json.splitlines()
+        predicted_classes = predictor.predict(inputs)
         print("predicted_classes: {}".format(predicted_classes))
 
-        for predicted_class_json, input_data in zip(predicted_classes, inputs):
-            predicted_class = json.loads(predicted_class_json)["predicted_label"]
+        #predicted_classes_str = response["Body"].read().decode()
+        #predicted_classes_json = json.loads(predicted_classes_str)
+        #predicted_classes_json = json.loads(response.decode())
+
+        #predicted_classes = predicted_classes_json.splitlines()
+        #print("predicted_classes: {}".format(predicted_classes))
+
+        for predicted_class_dict, input_data in zip(predicted_classes, inputs):
+            predicted_class = predicted_class_dict["predicted_label"]
             print('Predicted star_rating: {} for review_body "{}"'.format(predicted_class, input_data["features"][0]))
 
             # Built output_record
             # review_id, star_rating, product_category, review_body
             output_data = "{}\t{}\t{}\t{}".format(
-                split_inputs[0], str(predicted_class), split_inputs[1], input_data["review_body"]
+                split_inputs[0], str(predicted_class), split_inputs[1], input_data["features"]
             )
             print("output_data: {}".format(output_data))
             output_data_encoded = output_data.encode("utf-8")
