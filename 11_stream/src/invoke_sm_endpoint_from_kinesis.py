5935cfb5080703ceef9fe62fea58c7658b7d1bc5
Chris Fregly <chris@fregly.com>
2021-11-30 19:57:01 +0000

    updated back to invoke_endpoint

--- 11_stream/src/invoke_sm_endpoint_from_kinesis.py
+++ 11_stream/src/invoke_sm_endpoint_from_kinesis.py
@@ -5,23 +5,24 @@ import os
 import io
 import boto3
 import json
-from sagemaker.predictor import Predictor
-from sagemaker.serializers import JSONLinesSerializer
-from sagemaker.deserializers import JSONLinesDeserializer
+# from sagemaker.predictor import Predictor
+# from sagemaker.serializers import JSONLinesSerializer
+# from sagemaker.deserializers import JSONLinesDeserializer
 
 # grab environment variables
 ENDPOINT_NAME = os.environ["ENDPOINT_NAME"]
 print("Endpoint: {}".format(ENDPOINT_NAME))
+
 runtime = boto3.client("runtime.sagemaker")
 
-sess = sagemaker.Session()
+# sess = sagemaker.Session()
 
-predictor = Predictor(
-    endpoint_name=ENDPOINT_NAME,
-    serializer=JSONLinesSerializer(),
-    deserializer=JSONLinesDeserializer(),
-    sagemaker_session=sess
-)
+# predictor = Predictor(
+#     endpoint_name=ENDPOINT_NAME,
+#     serializer=JSONLinesSerializer(),
+#     deserializer=JSONLinesDeserializer(),
+#     sagemaker_session=sess
+# )
 
 
 def lambda_handler(event, context):
@@ -48,6 +49,13 @@ def lambda_handler(event, context):
 
         inputs = [{"features": [review_body]}]
 
+        response = runtime.invoke_endpoint(
+            EndpointName=ENDPOINT_NAME,
+            ContentType="application/jsonlines",
+            Accept="application/jsonlines",
+            Body=json.dumps(inputs).encode("utf-8"),
+        )
+            
         response = predictor.predict(inputs)
         print("response: {}".format(response))
 
