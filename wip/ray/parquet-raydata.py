05b8ad1b7b1f674b36b4842b141ce221837093fc
Antje Barth <23338582+antje@users.noreply.github.com>
2022-05-06 15:23:56 -0700

    updated with map batches

--- wip/ray/parquet-raydata.py
+++ wip/ray/parquet-raydata.py
@@ -4,5 +4,5 @@ ray.init(address="auto")
 
 df = ray.data.read_parquet("s3://dsoaws/parquet")
 
-print(df.groupby("product_category").count())
+print(df.map_batches(lambda row: row if row["product_category"] == "Books" else None)) #.groupby("product_category").count())
 print('blah')
