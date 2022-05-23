54c8d862731c6e09514202ed9f3bdcccdf7ecb9f
Chris Fregly <chris@fregly.com>
2022-05-22 22:24:25 -0400

    updated dockerfile

--- wip/ray/docker/build-and-push.sh
+++ wip/ray/docker/build-and-push.sh
@@ -2,6 +2,10 @@ docker build -t dsoaws/ray-fluxcapacitor:1.0 .
 
 docker tag dsoaws/ray-fluxcapacitor:1.0 079002598131.dkr.ecr.us-east-1.amazonaws.com/dsoaws/ray-fluxcapacitor:1.0
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 079002598131.dkr.ecr.us-east-1.amazonaws.com
+docker tag dsoaws/ray-fluxcapacitor:1.0 datascienceonaws/ray-fluxcapacitor:1.0
 
-docker push  079002598131.dkr.ecr.us-east-1.amazonaws.com/dsoaws/ray-fluxcapacitor:1.0
+docker push datascienceonaws/ray-fluxcapacitor:1.0
+
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 079002598131.dkr.ecr.us-east-1.amazonaws.com
+
+#docker push  079002598131.dkr.ecr.us-east-1.amazonaws.com/dsoaws/ray-fluxcapacitor:1.0
