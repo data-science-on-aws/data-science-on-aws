62300ce9a81829f6215d559aee20c75bc948529d
Chris Fregly <chris@fregly.com>
2022-06-07 00:38:50 -0400

    added more convenience functionality to restart script

--- wip/ray/restart.sh
+++ wip/ray/restart.sh
@@ -4,3 +4,4 @@ aws ssm delete-parameter --name AmazonCloudWatch-ray_agent_config_cluster
 ray up -y cluster.yaml
 ray exec cluster.yaml "git clone https://github.com/data-science-on-aws/data-science-on-aws.git"
 ray exec cluster.yaml "jupyter server list"
+nohup ray attach cluster.yaml -p 8888 > attach.out &
