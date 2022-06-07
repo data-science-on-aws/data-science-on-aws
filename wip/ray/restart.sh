60e0ad684daa16766bf47d0257937723e21a9600
Chris Fregly <chris@fregly.com>
2022-06-07 17:19:53 -0400

    updated instance account, added more comments to restart.sh

--- wip/ray/restart.sh
+++ wip/ray/restart.sh
@@ -3,5 +3,14 @@ aws ssm delete-parameter --name AmazonCloudWatch-ray_dashboard_config_cluster
 aws ssm delete-parameter --name AmazonCloudWatch-ray_agent_config_cluster
 ray up -y cluster.yaml
 ray exec cluster.yaml "git clone https://github.com/data-science-on-aws/data-science-on-aws.git"
+echo "JupyterLab..."
 ray exec cluster.yaml "jupyter server list"
-nohup ray attach cluster.yaml -p 8888 > attach.out &
+nohup ray attach cluster.yaml -p 8888 > attach-jupyterlab.out &
+nohup ray attach cluster.yaml -p 5001 > attach-mlflow.out &
+nohup ray dashboard cluster.yaml > dashboard.out &
+echo "CloudWatch Metrics..."
+echo "https://console.aws.amazon.com/cloudwatch/home?#dashboards:name=cluster-RayDashboard"
+echo ""
+echo "CloudWatch Logs..."
+echo "https://console.aws.amazon.com/cloudwatch/home?#logsV2:log-groups/log-group/cluster-ray_logs_out"
+echo "https://console.aws.amazon.com/cloudwatch/home?#logsV2:log-groups/log-group/cluster-ray_logs_err"
